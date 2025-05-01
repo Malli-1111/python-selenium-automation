1252d99909856d4ad2541a7af15f9d2eba9a69ea
Mallikarjun <152280419+Malli-1111@users.noreply.github.com>
2025-05-01 23:12:12 +0530

    Add Selenium script to automate dropdown selection using Select class
    This script demonstrates how to:
    - Open a local HTML file containing a dropdown or (open url that u have)
    - Locate the dropdown element by ID
    - Select options using visible text, value, and index methods of the Select class
    
    Useful for testing form controls and validating dropdown behavior in web applications.

---
+++ Selenium_scripts/10_Dropdown.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+
+# IF driver in current path ###
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(r'C:\Users\malli\OneDrive\Desktop\testingpractice\SELENIUM\DROP_DOWN.html')
+time.sleep(5)
+
+drop_element = driver.find_element(By.ID, 'MONTHS')
+
+months = Select(drop_element)
+
+time.sleep(5)
+
+## To select a value based on visable text
+months.select_by_visible_text('April')
+time.sleep(5)
+
+## To select a value based on value
+months.select_by_value('Feb')
+
+
+## To select a value based on index
+months.select_by_index(4)
+
+time.sleep(10)
+driver.close()   
